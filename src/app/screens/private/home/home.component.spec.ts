48b0d806a7487f8fdc4b63063a95abe8f6148c27
SantiagoAvila97 <santiagoavilapacanchique@gmail.com>
2025-08-20 20:23:13 -0500

    fix: :bug: error standalone components for unitary

--- src/app/screens/private/home/home.component.spec.ts
+++ src/app/screens/private/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +9,17 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [HomeComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({ id: '123' }),
+            snapshot: { paramMap: { get: () => '123' } },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
